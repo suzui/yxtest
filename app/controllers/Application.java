c11a0a44d0cde6ae921a2545af26aae5dd2c4a5f
郭常智 <inguochenfan@163.com>
2016-12-25 19:36:47 +0800

    commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,9 @@ package controllers;
 public class Application extends BaseController {
 
 	public static void index() {
+		if (request.getBase().endsWith("yixincishan.cn")) {
+			BackController.index();
+		}
 		render();
 	}
 
